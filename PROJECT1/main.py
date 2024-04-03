d896719e284dd2b8f2478217671b769514473680
Shitao Tang <tst17@my.swjtu.edu.cn>
2024-04-03 15:12:35 +0800

    characterized Chinese and English webpages, stored in res/(index).txt; update .gitignore

--- PROJECT1/main.py
+++ PROJECT1/main.py
@@ -1,31 +1,48 @@
 import os
 import re
+import zhconv
 
-def process_text(text):
-    # 删除字母，只保留中文和数字
-    processed_text = re.sub(r'[^\u4e00-\u9fa5\d]+', '', text)
-    return processed_text
+def get_character_cn(src_text):
+    dest_text = re.sub(r"[^\u4e00-\u9fa5]+", '', src_text)
+    # convert zh-tw/zh-hk/zh-hant to zh-cn
+    dest_text = zhconv.convert(dest_text, "zh-cn")
+    return dest_text
 
-def process_file(input_path, output_path):
-    with open(input_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        processed_content = process_text(content)
-    with open(output_path, 'w', encoding='utf-8') as file:
-        file.write(processed_content)
+def get_character_en(src_text):
+    dest_text = re.sub(r"[^A-Za-z]+", ' ', src_text)
+    return dest_text
 
-def process_directory(src_directory, dest_directory):
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
+def get_characterized_file(src_file, dest_file, get_character):
+    with open(src_file, 'r', encoding = "utf-8") as f:
+        dest_text = get_character(f.read())
+    with open(dest_file, 'w', encoding = "utf-8") as f:
+        f.write(dest_text)
 
-    for root, _, files in os.walk(src_directory):
-        for file_name in files:
-            input_path = os.path.join(root, file_name)
-            output_path = os.path.join(dest_directory, file_name)
-            process_file(input_path, output_path)
+dict_get_character = {
+    "en": get_character_en,
+    "cn": get_character_cn
+}
 
-if __name__ == "__main__":
-    src_directory = "downloaded/cn"
-    dest_directory = "processed_texts_chinese"
-    process_directory(src_directory, dest_directory)
-    print("Text processing completed.")
+def characterize(src_dir, dest_dir, type):
+    os.makedirs(dest_dir, exist_ok = True)
+    
+    files = os.listdir(src_dir)
+    index = 1
+    for file in files:
+        src_file = os.path.join(src_dir, file)
+        dest_file = os.path.join(dest_dir, f"{index}.txt")
+        # using dictionary to call function instead of "if...else..."
+        get_characterized_file(src_file, dest_file, dict_get_character[type])
+        index += 1
+
+
+# cn
+src_dir_cn = "downloaded/cn"
+dest_dir_cn = "res/cn"
+characterize(src_dir_cn, dest_dir_cn, "cn")
+
+#en
+src_dir_en = "downloaded/en"
+dest_dir_en = "res/en"
+characterize(src_dir_en, dest_dir_en, "en")
 
